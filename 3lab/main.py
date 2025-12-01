adbf286869e3da56aca8030e1238b1e707427aee
dostonibuston07 <dostonibuston.07@gmail.com>
2025-12-01 20:56:16 +0300

    Update main.py

--- 3lab/main.py
+++ 3lab/main.py
@@ -1,4 +1,47 @@
+def shortest_path(adj, start, end):
+    n = len(adj)
+    visited = [False] * n
+    log = []
+
+    def dfs(v, dist, path):
+        visited[v] = True
+        path.append(v)
+        log.append(("visit", v, dist, path.copy()))
+
+        if v == end:
+            visited[v] = False
+            path.pop()
+            return dist, path.copy()
+
+        best = (float('inf'), None)
+
+        for i, j in enumerate(adj[v]):
+            if j > 0 and not visited[i]:
+                log.append(("try", v, i, j, path.copy(), dist))
+                d, p = dfs(i, dist + j, path)
+                if d < best[0]:
+                    best = (d, p)
+                    log.append(("update_min", best))
+
+        visited[v] = False
+        path.pop()
+        return best
+
+    return (*dfs(start, 0, []), log)
 
 
-if __name__ == "__main__":
-    pass # Ваш код здесь
+# пример
+adj = [
+    [0, 3, 0, 7],
+    [3, 0, 2, 0],
+    [0, 2, 0, 1],
+    [7, 0, 1, 0]
+]
+
+dist, path, log = shortest_path(adj, 0, 3)
+
+print("distance:", dist)
+print("path:", path)
+print("\nlog:")
+for l in log:
+    print(l)
